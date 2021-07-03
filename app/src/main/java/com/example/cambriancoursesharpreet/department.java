5cfd1e232244a85f6fe8788d5e217bca8bc0eb33
A00230757 <84326658+A00230757@users.noreply.github.com>
2021-07-03 06:02:59 +0530

    today i made admin add department , view already added departments ,delete department .
    New Files made today.
    1.AddDepartmentActivity and xml
    2.single_row_department.xml
    3.departments.java
    
    appropriate variables names are used in both java and xml files.
    comments are not included , that will be there in the next commit.
    firebase realtime database with storage is used.

---
+++ app/src/main/java/com/example/cambriancoursesharpreet/department.java
@@ -0,0 +1,19 @@
+package com.example.cambriancoursesharpreet;
+
+public class department {
+    public String name;
+    public String description;
+    public String path;
+    department(){
+        name="";
+        description = "";
+    }
+    public department(String name,String description,String path)
+    {
+        this.name=name;
+        this.description = description;
+        this.path = path;
+    }
+
+
+}
