755c67f0159d3ef7943a96369d3eeca542d3988a
A00230757 <84326658+A00230757@users.noreply.github.com>
2021-07-05 04:42:17 +0530

    today i made admin add course in selected department, view already added courses in a particular department, delete course in selected department.
    New Files made today.
    1.AddCourseActivity and xml
    2.single_row_addcourse.xml
    3.course.java
    
    when this activity opens then few courses based on default selected department in top spinner are shown in courses list.
    And when admin change department then courses present in that particular department are displayed.
    Admin can view and delete already added courses.
    
    appropriate variables names are used in both java and xml files.
    firebase realtime database with storage is used to upload course data on cloud.

---
+++ app/src/main/java/com/example/cambriancoursesharpreet/firebase_cloud.java
@@ -0,0 +1,9 @@
+package com.example.cambriancoursesharpreet;
+
+public class firebase_cloud {
+    public String firebase_cloud_path ="https://cambriancoursesharpreet-default-rtdb.firebaseio.com/";
+
+    public String getLink(){
+        return firebase_cloud_path;
+    }
+}
